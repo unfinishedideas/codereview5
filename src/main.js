be6e8a4cd0fccf48ab0a01ecfcde8567f717b105
Peter Wells <petethebeatwells@gmail.com>
2019-11-15 09:26:25 -0800

    add front end interface to take a user age display results of ageCalc through the Super Galactic Age calculator

--- src/main.js
+++ src/main.js
@@ -2,3 +2,25 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { Person } from './person.js';
+
+$(document).ready(function(){
+  $(".userInputForm").submit(function(event){
+    event.preventDefault();
+    let ageInput = parseInt($("#userAge").val());
+    let newPerson = new Person(ageInput);
+    newPerson.calcAge();
+
+    // Display Results
+    $("#ageDump").text(newPerson.age);
+    $("#mercuryAgeDump").text(newPerson.mercuryAge);
+    $("#venusAgeDump").text(newPerson.venusAge);
+    $("#marsAgeDump").text(newPerson.marsAge);
+    $("#jupiterAgeDump").text(newPerson.jupiterAge);
+
+    $(".userInputForm").hide();
+    $(".resultsBox").show();
+
+  });
+
+});
