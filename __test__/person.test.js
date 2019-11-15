d259da13ab78612d5c7845019f60b08c0d3c9705
Peter Wells <petethebeatwells@gmail.com>
2019-11-15 08:51:23 -0800

    add getAge method to player objects which works with NaN test then write new spec to test for negative numbers

--- __test__/person.test.js
+++ __test__/person.test.js
@@ -3,9 +3,13 @@ import { Person } from './../src/person.js';
 describe('Person', () => {
 
   test('should check if persons age is a number', () => {
-    var newPerson = new Person("sup");
+    var newPerson = new Person(NaN);
     expect(newPerson.calcAge()).toEqual("Err: NaN");
   });
 
+  test('should check if persons age is a positive integer', () => {
+    var newPerson = new Person(-2000);
+    expect(newPerson.calcAge()).toEqual("Err: Negative Number");
+  });
 
 });
--- src/person.js
+++ src/person.js
@@ -2,4 +2,9 @@ export class Person {
   constructor (age){
     this.age = age;
   }
+  calcAge(){
+    if (Number.isNaN(this.age)){
+      return "Err: NaN";
+    }
+  }
 }
